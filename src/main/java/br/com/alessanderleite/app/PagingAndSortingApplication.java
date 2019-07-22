ee9e1d255ff22c5e95a675773be080a1416afefd
alessanderleite <alessander.logistica@gmail.com>
2019-07-21 21:50:21 -0300

    call the ClientRepository inside application class

--- src/main/java/br/com/alessanderleite/app/PagingAndSortingApplication.java
+++ src/main/java/br/com/alessanderleite/app/PagingAndSortingApplication.java
@@ -1,11 +1,17 @@
 package br.com.alessanderleite.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.alessanderleite.app.dao.ClientRepository;
 
 @SpringBootApplication
 public class PagingAndSortingApplication {
 
+	@Autowired
+	ClientRepository clientRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PagingAndSortingApplication.class, args);
 	}
